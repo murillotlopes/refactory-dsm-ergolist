fadecc8bddb586b1647064b10d02565b3a9df40e
Lucas Ribeiro <lucassopessoal@gmail.com>
2022-06-07 20:27:24 -0300

    (07/06) carrossel
    Inserção do carrossel na página inicial.

--- front-end/src/components/Carrossel/index.jsx
+++ front-end/src/components/Carrossel/index.jsx
@@ -1,11 +1,27 @@
 import { ContainerCarrossel } from "./style"
 
+import img from './download.png'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+
+import { Carousel } from 'react-responsive-carousel';
+
 const Carrocel = () => {
     return(
         <ContainerCarrossel>
-
-
-            
+            <div>
+            <Carousel showThumbs={false} showIndicators={false} showStatus={false} dynamicHeight={false} autoPlay={true} infiniteLoop={true}>
+                <div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae totam similique laborum aut libero odio quaerat, illo cumque et aperiam. Eaque saepe odit odio omnis quaerat molestias provident fugiat voluptas? Lore</p>
+                </div>
+                <div>
+                   <p> Seja bem-vindo ao Ergoframe, o site amigo das interfaces</p>
+                </div>
+                <div>
+                    <p> Seja bem-vindo ao Ergoframe, o site amigo das interfaces</p>
+                </div>
+            </Carousel>
+            </div>
         </ContainerCarrossel>
     )
 }
--- front-end/src/components/Carrossel/style.js
+++ front-end/src/components/Carrossel/style.js
@@ -2,4 +2,18 @@ import styled from 'styled-components'
 
 export const ContainerCarrossel = styled.div`
     // box-shadow: 1px 1px 10px 1px black;
+
+    display: block;
+
+    p {
+        margin: 25% 3em;
+        font-size: 18px;
+        text-align: justify;
+    }
+
+    @mixin for-big-desktop-up {
+        @media (min-width: 1000px) { 
+            p { font-size: 64px } }
+      }
+
 `
