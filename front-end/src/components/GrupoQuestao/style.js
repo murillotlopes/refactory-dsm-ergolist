ae8e5afce1f8ea1f0db840a078704f756ccf307e
Lucas Ribeiro <lucassopessoal@gmail.com>
2022-06-09 20:37:54 -0300

    redimensionamento dos cards

--- front-end/src/components/GrupoQuestao/style.js
+++ front-end/src/components/GrupoQuestao/style.js
@@ -4,7 +4,7 @@ export const GroupCardStyle = styled.div`
     height: 250px;
     width: 250px;
 
-    margin: 25px 40px;
+    margin: 25px 40px 25px 0;
     padding: 30px;
 
     background-color: gray;
@@ -14,4 +14,6 @@ export const GroupCardStyle = styled.div`
     box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
 
     grid-template: 100px 1fr / 50px 1fr;
+
+    max-width: 100%;
 `
--- front-end/src/pages/GroupQuestion/style.js
+++ front-end/src/pages/GroupQuestion/style.js
@@ -3,8 +3,9 @@ import styled from "styled-components";
 export const ContainerGroupQuestion = styled.div`
     display: grid;
     
-    grid-template-columns: minmax(275px, max-content)
-    repeat(4, 275px) 20%;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 2fr));
+
+    margin: 45px;
     div {
     }
 `
