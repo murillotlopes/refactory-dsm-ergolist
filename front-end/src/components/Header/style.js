3c5b7b336877ada2c0257e64c4b256e9e1ccc779
valeriabarci <vahc.201@gmail.com>
2022-06-14 16:49:26 -0300

    inserção do logo no header

--- front-end/src/components/Header/style.js
+++ front-end/src/components/Header/style.js
@@ -6,26 +6,12 @@ export const ContainerHeader = styled.header`
     display: flex;
     justify-content: center ;
     align-items: center;
-    background-color: var(--primary-red);
+    background-color: var(--primary-red-dark);
 
     margin-bottom: 40px;
 
-    h1 {
-        //box-shadow: 2px 2px 10px var(--white);
-
-        padding: 13px;
-        //border-radius: var(--radius) ;
+    img {
+        width: 70px;
     }
 
-    /* h1:active{
-        //background-color: var(--primary-red-dark) ;
-        box-shadow: 1px 1px 10px var(--white);
-    } */
-    
-    h1 a{
-        color: var(--white);
-        font-family: 'Roboto Slab', serif;
-        font-weight: 400;
-        text-decoration: none;
-    }
 `
