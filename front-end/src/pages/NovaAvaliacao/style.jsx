096ba70f8d182156c3e51bee333bf1e9516bf4a0
valeriabarci <vahc.201@gmail.com>
2022-06-18 19:29:30 -0300

    Estilização página Nova Avaliação

--- front-end/src/pages/NovaAvaliacao/style.jsx
+++ front-end/src/pages/NovaAvaliacao/style.jsx
@@ -7,4 +7,12 @@ export const ContainerNovaAvaliacao = styled.div`
 
     border-radius: var(--radius);
     box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+
+    button {
+        display: block;
+        cursor: pointer;
+        text-align: center;
+        width: 250px;
+        margin-bottom: 20px;
+    }
 `
