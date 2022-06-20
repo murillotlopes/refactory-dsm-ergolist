e709c9b9c33c0077a65be78892c45ba79bb676a6
Lucas-Ribeir0 <lucassopessoal@gmail.com>
2022-06-20 01:45:40 +0000

    pequena alteracao carrossel

--- front-end/src/components/Carrossel/index.jsx
+++ front-end/src/components/Carrossel/index.jsx
@@ -1,4 +1,4 @@
-import { ContainerCarrossel } from "./style"
+import { BgImage, ContainerCarrossel } from "./style"
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
@@ -8,19 +8,25 @@ import { Carousel } from 'react-responsive-carousel';
 const Carrocel = () => {
     return(
         <ContainerCarrossel>
-            <div>
-            <Carousel showThumbs={false} showIndicators={false} showStatus={false} dynamicHeight={false} autoPlay={true} infiniteLoop={true} interval={2500}>
+            <BgImage>
+            <Carousel showArrows={false} showThumbs={false} showIndicators={false} showStatus={false} dynamicHeight={false} autoPlay={true} infiniteLoop={true} interval={3500}>
                 <div>
-                    <p>Seja bem-vindo ao Ergoframe, o site que busca ajudá-lo na auto-avaliação de suas telas usando metódos de avaliação ergonômicos propostos por Bastien e Scapin.</p>
+                    <p>Seja bem-vindo ao Ergoframe, o site que ajuda na auto-avaliação de suas telas usando metódos de avaliação ergonômicos propostos por Bastien e Scapin.</p>
+                </div>
+                <div>
+                    <p>A avaliação ergônomica consiste um questionário baseado em avaliações heurísticas, as quais tem foco na usabilidade da interface que está sendo avaliada, portanto, mostra quais trechos podem ser melhorados. </p>
+                </div>
+                <div>
+                    <p>Ao criar sua conta, suas avaliações permanecerão salvas e poderão ser utlizadas ou compartilhadas quando quiser!</p>
+                </div>
+                <div>
+                    <p>Suas avaliações serão salvas a cada questão respondida, assim, todo seu progresso estará seguro conosco.</p>
                 </div>
                 <div>
                    <p>Nosso projeto se basea na criação do Ergolist. Uma colaboração do Núcleo Softex-2000 de Florianópolis e o Laboratório de Utizabilidade, (UFSC/SENAI-SC/CTAI) no ano de 1999. </p>
                 </div>
-                <div>
-                    <p></p>
-                </div>
             </Carousel>
-            </div>
+            </BgImage>
         </ContainerCarrossel>
     )
 }
--- front-end/src/components/Carrossel/style.js
+++ front-end/src/components/Carrossel/style.js
@@ -1,14 +1,28 @@
 import styled from 'styled-components'
+import img from '../../assets/images/logo_footer.png'
 
 export const ContainerCarrossel = styled.div`
     // box-shadow: 1px 1px 10px 1px black;
 
-    display: block;
+    font-family: 'Roboto Slab', serif;
 
     p {
-        margin: 25% 3em;
+        margin: 25% 5em;
         font-size: 18px;
         text-align: justify;
     }
 
+    button {
+        height: 100%;
+    }
+    
+    `
+    
+    export const BgImage = styled.div`
+    
+    background: url(${img}); 
+    background-size: 240px 240px;
+    background-repeat: no-repeat;
+    background-position: center;
+    z-index: -1;
 `
