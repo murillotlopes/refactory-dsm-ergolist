6a07533c501b41498eded06ddd2734b303e2d2b0
murillotlopes <murillo.tlopes@hotmail.com>
2024-08-08 14:33:41 -0300

    refactor(backend - config): refactoring to typeScript

--- back-end/config/database.ts
+++ back-end/config/database.ts
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-module.exports = function(){
+import mongoose from "mongoose"
+
+export default function database() {
     mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERV}/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
         // Parâmetros da conexão
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
         // useFindAndModify: false
     })
 
 
-    mongoose.connection.on('connected', () => 
-    console.log('** Mongoose! conectado ao servidor remoto')
+    mongoose.connection.on('connected', () =>
+        console.log('** Mongoose! conectado ao servidor remoto')
     )
 
-    mongoose.connection.on('error', erro => 
-    console.error('** ERROR: Mongooose! Não conectado ao servidor remoto, Causa: ' + erro)
+    mongoose.connection.on('error', (erro: any) =>
+        console.error('** ERROR: Mongooose! Não conectado ao servidor remoto, Causa: ' + erro)
     )
 }
