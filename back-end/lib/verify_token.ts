2ac236e0b93d5c3da5f517433b020ad64a337587
murillotlopes <murillo.tlopes@hotmail.com>
2024-08-15 15:21:04 -0300

    feat(back - verify_token): change in token capture in request header

--- back-end/lib/verify_token.ts
+++ back-end/lib/verify_token.ts
@@ -3,13 +3,15 @@ import { verify } from 'jsonwebtoken'
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     // Lê o token passado no cabeçalho da requisição
-    const token = req.headers['x-access-token'] as string
+    const token = req.headers['Authorization'] as string
 
     // Se o token não existir, retorna 403: Forbidden
     if (!token) return res.status(403).send({ auth: false, message: 'No token provided' })
 
+    if (token.split(' ').length !== 2) return res.status(403).send({ auth: false, message: 'No token provided' })
+
     // Verifica se o token é válido e está no prazo de validade
-    verify(token, process.env.SECRET as string, (err, decoded: any) => {
+    verify(token.split(' ')[1], process.env.SECRET as string, (err, decoded: any) => {
         // Token inválido/expirado
         if (err) return res.status(403).send({ auth: false, message: 'Failed to authenticate token' })
 
